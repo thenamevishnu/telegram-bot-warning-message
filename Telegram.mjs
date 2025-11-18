de0d86ed1115878efa65a1dbe6e6bda3971a9b6d
VISHNU M K <mail.vishnumk@gmail.com>
2025-11-18 17:57:24 +0530

    Updated

--- Telegram.mjs
+++ Telegram.mjs
@@ -4,17 +4,13 @@ const api = new TelegramBot(process.env.BOT_TOKEN, {
     polling: true
 })
 
-const userMention = (from) => {
-    return from.username ? `@${from.username}` : `<a href="tg://user?id=${from.id}">${from.first_name}</a>`
-}
-
 api.on("message", async msg => {
     const text = `<b>⚠️ Important Update\nThis bot has been deprecated and is no longer active. We’ve shifted everything to a newer and more advanced system so you can enjoy smoother performance and better features.\n\n🚀 Move to the New Bot\nTo continue accessing tasks, referrals, rewards, and all future updates, please switch to our upgraded bot.\n\n👉 Join @TrueClickBot to continue\nEverything happens there now — faster, cleaner, and fully updated.\n\n✨ See you on the new bot!</b>`
     return await api.sendMessage(msg.chat.id, text, {
         parse_mode: "HTML",
         disable_web_page_preview: true,
         reply_to_message_id: msg.message_id,
-        protect_content: true,
+        protect_content: msg.from.id != process.env.ADMIN_ID,
         reply_markup: {
             inline_keyboard: [
                 [
